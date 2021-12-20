5e3933f8ea620d7c89711e33b9855da63ca009ba
shivam-j2ee <tiwari.shivam26@gmail.com>
2021-12-20 16:37:17 +0530

    springbootfirstcommit

---
+++ src/main/java/com/kashyap/cricketmanagement/CricketmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.kashyap.cricketmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashyap/cricketmanagement/CricketmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashyap.cricketmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
