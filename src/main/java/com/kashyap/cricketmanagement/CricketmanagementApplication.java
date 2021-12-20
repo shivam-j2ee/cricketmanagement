17d36ea5428f104a4dbc931b30624394b74a0ce8
shivam-j2ee <tiwari.shivam26@gmail.com>
2021-12-20 17:27:21 +0530

    changes in main file

--- src/main/java/com/kashyap/cricketmanagement/CricketmanagementApplication.java
+++ src/main/java/com/kashyap/cricketmanagement/CricketmanagementApplication.java
@@ -8,6 +8,7 @@ public class CricketmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CricketmanagementApplication.class, args);
+		System.out.println("New Project");
 	}
 
 }
